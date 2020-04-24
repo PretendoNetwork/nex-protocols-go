616e87b6f4fc1248f9630c2ca9b2c8acfd8aad9c
Jonathan Barrow <jonbarrow1998@gmail.com>
2020-04-24 13:53:21 -0400

    Updated matchmaking protocol

--- matchmake_extension.go
+++ matchmake_extension.go
@@ -1,6 +1,7 @@
 package nexproto
 
 import (
+	"errors"
 	"fmt"
 
 	nex "github.com/PretendoNetwork/nex-go"
@@ -10,14 +11,17 @@ const (
 	// MatchmakeExtensionProtocolID is the protocol ID for the Matchmake Extension protocol
 	MatchmakeExtensionProtocolID = 0x6D
 
+	// MatchmakeExtensionMethodCreateMatchmakeSession is the method ID for method CreateMatchmakeSession
+	MatchmakeExtensionMethodCreateMatchmakeSession = 0x6
+
 	// MatchmakeExtensionMethodGetSimplePlayingSession is the method ID for method GetSimplePlayingSession
 	MatchmakeExtensionMethodGetSimplePlayingSession = 0x1F
 )
 
 // MatchmakeExtensionProtocol handles the Matchmake Extension nex protocol
 type MatchmakeExtensionProtocol struct {
-	server *nex.Server
-
+	server                         *nex.Server
+	CreateMatchmakeSessionHandler  func(err error, client *nex.Client, callID uint32, matchmakeSession *MatchmakeSession, message string, participationCount uint16)
 	GetSimplePlayingSessionHandler func(err error, client *nex.Client, callID uint32, listPID []uint32, includeLoginUser bool)
 }
 
@@ -26,22 +30,96 @@ func (matchmakeExtensionProtocol *MatchmakeExtensionProtocol) Setup() {
 	nexServer := matchmakeExtensionProtocol.server
 
 	nexServer.On("Data", func(packet nex.PacketInterface) {
-		request := packet.GetRMCRequest()
+		request := packet.RMCRequest()
 
-		if MatchmakeExtensionProtocolID == request.GetProtocolID() {
-			switch request.GetMethodID() {
+		if MatchmakeExtensionProtocolID == request.ProtocolID() {
+			switch request.MethodID() {
+			case MatchmakeExtensionMethodCreateMatchmakeSession:
+				go matchmakeExtensionProtocol.handleCreateMatchmakeSession(packet)
 			case MatchmakeExtensionMethodGetSimplePlayingSession:
 				go matchmakeExtensionProtocol.handleGetSimplePlayingSession(packet)
 			default:
-				fmt.Printf("Unsupported Matchmake Extension method ID: %#v\n", request.GetMethodID())
+				fmt.Printf("Unsupported Matchmake Extension method ID: %#v\n", request.MethodID())
 			}
 		}
 	})
 }
 
+// CreateMatchmakeSession sets the CreateMatchmakeSession handler function
+func (matchmakeExtensionProtocol *MatchmakeExtensionProtocol) CreateMatchmakeSession(handler func(err error, client *nex.Client, callID uint32, matchmakeSession *MatchmakeSession, message string, participationCount uint16)) {
+	matchmakeExtensionProtocol.CreateMatchmakeSessionHandler = handler
+}
+
 // GetSimplePlayingSession sets the GetSimplePlayingSession handler function
 func (matchmakeExtensionProtocol *MatchmakeExtensionProtocol) GetSimplePlayingSession(handler func(err error, client *nex.Client, callID uint32, listPID []uint32, includeLoginUser bool)) {
 	matchmakeExtensionProtocol.GetSimplePlayingSessionHandler = handler
+}
+
+func (matchmakeExtensionProtocol *MatchmakeExtensionProtocol) handleCreateMatchmakeSession(packet nex.PacketInterface) {
+	if matchmakeExtensionProtocol.CreateMatchmakeSessionHandler == nil {
+		fmt.Println("[Warning] MatchmakeExtensionProtocol::CreateMatchmakeSession not implemented")
+		go respondNotImplemented(packet, MatchmakeExtensionProtocolID)
+		return
+	}
+
+	client := packet.Sender()
+	request := packet.RMCRequest()
+
+	callID := request.CallID()
+	parameters := request.Parameters()
+
+	parametersStream := nex.NewStreamIn(parameters, matchmakeExtensionProtocol.server)
+
+	dataHolderType, err := parametersStream.ReadString()
+
+	if err != nil {
+		go matchmakeExtensionProtocol.CreateMatchmakeSessionHandler(err, client, callID, nil, "", 0)
+		return
+	}
+
+	if dataHolderType != "MatchmakeSession" {
+		err := errors.New("[MatchmakeExtensionProtocol::CreateMatchmakeSession] Data holder name does not match")
+		go matchmakeExtensionProtocol.CreateMatchmakeSessionHandler(err, client, callID, nil, "", 0)
+		return
+	}
+
+	if (parametersStream.ByteCapacity() - parametersStream.ByteOffset()) < 8 {
+		err := errors.New("[MatchmakeExtensionProtocol::CreateMatchmakeSession] Data holder missing lengths")
+		go matchmakeExtensionProtocol.CreateMatchmakeSessionHandler(err, client, callID, nil, "", 0)
+		return
+	}
+
+	parametersStream.SeekByte(4, true) // Skip length including next buffer length field
+	dataHolderContent, err := parametersStream.ReadBuffer()
+
+	if err != nil {
+		go matchmakeExtensionProtocol.CreateMatchmakeSessionHandler(err, client, callID, nil, "", 0)
+		return
+	}
+
+	dataHolderContentStream := nex.NewStreamIn(dataHolderContent, matchmakeExtensionProtocol.server)
+
+	matchmakeSession, err := dataHolderContentStream.ReadStructure(NewMatchmakeSession())
+
+	if err != nil {
+		go matchmakeExtensionProtocol.CreateMatchmakeSessionHandler(err, client, callID, nil, "", 0)
+		return
+	}
+
+	message, err := dataHolderContentStream.ReadString()
+
+	if err != nil {
+		go matchmakeExtensionProtocol.CreateMatchmakeSessionHandler(err, client, callID, nil, "", 0)
+		return
+	}
+
+	var participationCount uint16 = 0
+
+	if matchmakeExtensionProtocol.server.NexVersion() >= 30500 {
+		participationCount = dataHolderContentStream.ReadUInt16LE()
+	}
+
+	go matchmakeExtensionProtocol.CreateMatchmakeSessionHandler(nil, client, callID, matchmakeSession.(*MatchmakeSession), message, participationCount)
 }
 
 func (matchmakeExtensionProtocol *MatchmakeExtensionProtocol) handleGetSimplePlayingSession(packet nex.PacketInterface) {
@@ -51,11 +129,11 @@ func (matchmakeExtensionProtocol *MatchmakeExtensionProtocol) handleGetSimplePla
 		return
 	}
 
-	client := packet.GetSender()
-	request := packet.GetRMCRequest()
+	client := packet.Sender()
+	request := packet.RMCRequest()
 
-	callID := request.GetCallID()
-	parameters := request.GetParameters()
+	callID := request.CallID()
+	parameters := request.Parameters()
 
 	parametersStream := nex.NewStreamIn(parameters, matchmakeExtensionProtocol.server)
 
---
+++ matchmaking_types.go
@@ -0,0 +1,181 @@
+package nexproto
+
+import nex "github.com/PretendoNetwork/nex-go"
+
+/*
+	NEX and Rendez-Vous have multiple protocols for match making
+	These protocols all share the same types
+	In an effort to keep this library organized, each type used in all match making protocols is defined here
+*/
+
+// Gathering holds information about a matchmake gathering
+type Gathering struct {
+	ID                  uint32
+	OwnerPID            uint32
+	HostPID             uint32
+	MinimumParticipants uint16
+	MaximumParticipants uint16
+	ParticipationPolicy uint32
+	PolicyArgument      uint32
+	Flags               uint32
+	State               uint32
+	Description         string
+
+	*nex.Structure
+}
+
+// ExtractFromStream extracts a Gathering structure from a stream
+func (gathering *Gathering) ExtractFromStream(stream *nex.StreamIn) error {
+	var err error
+
+	gathering.ID = stream.ReadUInt32LE()
+	gathering.OwnerPID = stream.ReadUInt32LE()
+	gathering.HostPID = stream.ReadUInt32LE()
+	gathering.MinimumParticipants = stream.ReadUInt16LE()
+	gathering.MaximumParticipants = stream.ReadUInt16LE()
+	gathering.ParticipationPolicy = stream.ReadUInt32LE()
+	gathering.PolicyArgument = stream.ReadUInt32LE()
+	gathering.Flags = stream.ReadUInt32LE()
+	gathering.State = stream.ReadUInt32LE()
+	gathering.Description, err = stream.ReadString()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// NewGathering returns a new Gathering
+func NewGathering() *Gathering {
+	return &Gathering{}
+}
+
+// MatchmakeSession holds information about a matchmake session
+type MatchmakeSession struct {
+	GameMode              uint32
+	Attributes            []uint32
+	OpenParticipation     bool
+	MatchmakeSystemType   uint32
+	ApplicationData       []byte
+	ParticipationCount    uint32
+	ProgressScore         uint8           // NEX v3.5.0+
+	SessionKey            []byte          // NEX v3.0.0+
+	Option                uint32          // NEX v3.5.0+
+	MatchmakeParam        *MatchmakeParam // NEX v4.0.0+
+	StartedTime           *nex.DateTime   // NEX v4.0.0+
+	UserPassword          string          // NEX v4.0.0+
+	ReferGID              uint32          // NEX v4.0.0+
+	UserPasswordEnabled   bool            // NEX v4.0.0+
+	SystemPasswordEnabled bool            // NEX v4.0.0+
+	CodeWord              string          // NEX v4.0.0+
+
+	hierarchy []nex.StructureInterface
+	*Gathering
+}
+
+// GetHierarchy returns the Structure hierarchy
+func (matchmakeSession *MatchmakeSession) GetHierarchy() []nex.StructureInterface {
+	return matchmakeSession.hierarchy
+}
+
+// ExtractFromStream extracts a MatchmakeSession structure from a stream
+func (matchmakeSession *MatchmakeSession) ExtractFromStream(stream *nex.StreamIn) error {
+	var err error
+	server := stream.Server
+
+	matchmakeSession.GameMode = stream.ReadUInt32LE()
+	matchmakeSession.Attributes = stream.ReadListUInt32LE()
+	matchmakeSession.OpenParticipation = stream.ReadUInt8() == 1
+	matchmakeSession.MatchmakeSystemType = stream.ReadUInt32LE()
+	matchmakeSession.ApplicationData, err = stream.ReadBuffer()
+
+	if err != nil {
+		return err
+	}
+
+	matchmakeSession.ParticipationCount = stream.ReadUInt32LE()
+
+	if server.NexVersion() >= 30500 {
+		matchmakeSession.ProgressScore = stream.ReadUInt8()
+	}
+
+	if server.NexVersion() >= 30000 {
+		matchmakeSession.SessionKey, err = stream.ReadBuffer()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if server.NexVersion() >= 30500 {
+		matchmakeSession.Option = stream.ReadUInt32LE()
+	}
+
+	if server.NexVersion() >= 40000 {
+		matchmakeParam, err := stream.ReadStructure(NewMatchmakeParam())
+
+		if err != nil {
+			return err
+		}
+
+		matchmakeSession.MatchmakeParam = matchmakeParam.(*MatchmakeParam)
+		matchmakeSession.StartedTime = nex.NewDateTime(stream.ReadUInt64LE())
+		matchmakeSession.UserPassword, err = stream.ReadString()
+
+		if err != nil {
+			return err
+		}
+
+		matchmakeSession.ReferGID = stream.ReadUInt32LE()
+		matchmakeSession.UserPasswordEnabled = stream.ReadUInt8() == 1
+		matchmakeSession.SystemPasswordEnabled = stream.ReadUInt8() == 1
+		matchmakeSession.CodeWord, err = stream.ReadString()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// NewMatchmakeSession returns a new MatchmakeSession
+func NewMatchmakeSession() *MatchmakeSession {
+	matchmakeSession := &MatchmakeSession{}
+
+	gathering := NewGathering()
+
+	matchmakeSession.Gathering = gathering
+
+	matchmakeSession.hierarchy = []nex.StructureInterface{
+		gathering,
+	}
+
+	return matchmakeSession
+}
+
+// MatchmakeParam holds parameters for a matchmake session
+type MatchmakeParam struct {
+	parameters map[interface{}]interface{}
+
+	*nex.Structure
+}
+
+// ExtractFromStream extracts a MatchmakeParam structure from a stream
+func (matchmakeParam *MatchmakeParam) ExtractFromStream(stream *nex.StreamIn) error {
+	var err error
+
+	matchmakeParam.parameters, err = stream.ReadMap(stream.ReadString, stream.ReadVariant)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// NewMatchmakeParam returns a new MatchmakeParam
+func NewMatchmakeParam() *MatchmakeParam {
+	return &MatchmakeParam{}
+}

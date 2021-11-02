0096666dfe3a0d2f6e072c104f50ecf390abbc17
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-11-02 18:44:09 -0400

    Added NintendoNotificationEventGeneral

--- nintendo_notifications.go
+++ nintendo_notifications.go
@@ -20,6 +20,30 @@ type NintendoNotificationsProtocol struct {
 	server *nex.Server
 }
 
+// NintendoNotificationEventGeneral holds general purpose notification data
+type NintendoNotificationEventGeneral struct {
+	nex.Structure
+	U32Param  uint32
+	U64Param1 uint64
+	U64Param2 uint64
+	StrParam  string
+}
+
+// Bytes encodes the NintendoNotificationEventGeneral and returns a byte array
+func (nintendoNotificationEventGeneral *NintendoNotificationEventGeneral) Bytes(stream *nex.StreamOut) []byte {
+	stream.WriteUInt32LE(nintendoNotificationEventGeneral.U32Param)
+	stream.WriteUInt64LE(nintendoNotificationEventGeneral.U64Param1)
+	stream.WriteUInt64LE(nintendoNotificationEventGeneral.U64Param2)
+	stream.WriteString(nintendoNotificationEventGeneral.StrParam)
+
+	return stream.Bytes()
+}
+
+// NintendoNotificationEventGeneral returns a new NintendoNotificationEventGeneral
+func NewNintendoNotificationEventGeneral() *NintendoNotificationEventGeneral {
+	return &NintendoNotificationEventGeneral{}
+}
+
 // NintendoNotificationEvent is used to send data about a notification event to a client
 type NintendoNotificationEvent struct {
 	nex.Structure

9e5c87836a5166f23d98a831ec58a1d875f911b3
shutterbug2000 <shutterbug90002@gmail.com>
2022-09-05 03:23:43 +0000

    Add UpdateSessionHostV1 and UpdateSessionHost

--- matchmaking.go
+++ matchmaking.go
@@ -19,8 +19,14 @@ const (
 	// MatchMakingMethodFindBySingleID is the method ID for the method FindBySingleID
 	MatchMakingMethodFindBySingleID = 0x15
 
+	// MatchMakingMethodUpdateSessionHostV1 is the method ID for the method UpdateSessionHostV1
+	MatchMakingMethodUpdateSessionHostV1 = 0x28
+
 	// MatchMakingMethodGetSessionURLs is the method ID for the method GetSessionURLs
 	MatchMakingMethodGetSessionURLs = 0x29
+
+	// MatchMakingMethodUpdateSessionHost is the method ID for the method UpdateSessionHost
+	MatchMakingMethodUpdateSessionHost = 0x2A
 )
 
 // AuthenticationProtocol handles the Authentication nex protocol
@@ -29,7 +35,9 @@ type MatchMakingProtocol struct {
 	UnregisterGatheringHandler  func(err error, client *nex.Client, callID uint32, idGathering uint32)
 	UnregisterGatheringsHandler func(err error, client *nex.Client, callID uint32, lstGatherings []uint32)
 	FindBySingleIDHandler       func(err error, client *nex.Client, callID uint32, id uint32)
+	UpdateSessionHostV1Handler func(err error, client *nex.Client, callID uint32, gatheringId uint32)
 	GetSessionURLsHandler       func(err error, client *nex.Client, callID uint32, gatheringId uint32)
+	UpdateSessionHostHandler   func(err error, client *nex.Client, callID uint32, gatheringId uint32)
 }
 
 // Setup initializes the protocol
@@ -47,8 +55,12 @@ func (matchMakingProtocol *MatchMakingProtocol) Setup() {
 				go matchMakingProtocol.handleMatchMakingMethodUnregisterGatherings(packet)
 			case MatchMakingMethodFindBySingleID:
 				go matchMakingProtocol.handleFindBySingleID(packet)
+			case MatchMakingMethodUpdateSessionHostV1:
+				go matchMakingProtocol.handleUpdateSessionHostV1(packet)
 			case MatchMakingMethodGetSessionURLs:
 				go matchMakingProtocol.handleGetSessionURLs(packet)
+			case MatchMakingMethodUpdateSessionHost:
+				go matchMakingProtocol.handleUpdateSessionHost(packet)
 			default:
 				go respondNotImplemented(packet, MatchMakingProtocolID)
 				fmt.Printf("Unsupported MatchMaking method ID: %#v\n", request.MethodID())
@@ -73,8 +85,18 @@ func (matchMakingProtocol *MatchMakingProtocol) FindBySingleID(handler func(err 
 }
 
 // GetSessionURLs sets the GetSessionURLs handler function
+func (matchMakingProtocol *MatchMakingProtocol) UpdateSessionHostV1(handler func(err error, client *nex.Client, callID uint32, gatheringId uint32)) {
+	matchMakingProtocol.UpdateSessionHostV1Handler = handler
+}
+
+// GetSessionURLs sets the GetSessionURLs handler function
 func (matchMakingProtocol *MatchMakingProtocol) GetSessionURLs(handler func(err error, client *nex.Client, callID uint32, gatheringId uint32)) {
 	matchMakingProtocol.GetSessionURLsHandler = handler
+}
+
+// GetSessionURLs sets the GetSessionURLs handler function
+func (matchMakingProtocol *MatchMakingProtocol) UpdateSessionHost(handler func(err error, client *nex.Client, callID uint32, gatheringId uint32)) {
+	matchMakingProtocol.UpdateSessionHostHandler = handler
 }
 
 func (matchMakingProtocol *MatchMakingProtocol) handleMatchMakingMethodUnregisterGathering(packet nex.PacketInterface) {
@@ -137,6 +159,26 @@ func (matchMakingProtocol *MatchMakingProtocol) handleFindBySingleID(packet nex.
 	go matchMakingProtocol.FindBySingleIDHandler(nil, client, callID, id)
 }
 
+func (matchMakingProtocol *MatchMakingProtocol) handleUpdateSessionHostV1(packet nex.PacketInterface) {
+	if matchMakingProtocol.UpdateSessionHostV1Handler == nil {
+		fmt.Println("[Warning] MatchMakingProtocol::UpdateSessionHostV1 not implemented")
+		go respondNotImplemented(packet, MatchMakingProtocolID)
+		return
+	}
+
+	client := packet.Sender()
+	request := packet.RMCRequest()
+
+	callID := request.CallID()
+	parameters := request.Parameters()
+
+	parametersStream := nex.NewStreamIn(parameters, matchMakingProtocol.server)
+
+	gatheringId := parametersStream.ReadUInt32LE()
+
+	go matchMakingProtocol.UpdateSessionHostV1Handler(nil, client, callID, gatheringId)
+}
+
 func (matchMakingProtocol *MatchMakingProtocol) handleGetSessionURLs(packet nex.PacketInterface) {
 	if matchMakingProtocol.GetSessionURLsHandler == nil {
 		logger.Warning("MatchMakingProtocol::GetSessionURLs not implemented")
@@ -155,6 +197,26 @@ func (matchMakingProtocol *MatchMakingProtocol) handleGetSessionURLs(packet nex.
 	gatheringId := parametersStream.ReadUInt32LE()
 
 	go matchMakingProtocol.GetSessionURLsHandler(nil, client, callID, gatheringId)
+}
+
+func (matchMakingProtocol *MatchMakingProtocol) handleUpdateSessionHost(packet nex.PacketInterface) {
+	if matchMakingProtocol.UpdateSessionHostHandler == nil {
+		fmt.Println("[Warning] MatchMakingProtocol::UpdateSessionHost not implemented")
+		go respondNotImplemented(packet, MatchMakingProtocolID)
+		return
+	}
+
+	client := packet.Sender()
+	request := packet.RMCRequest()
+
+	callID := request.CallID()
+	parameters := request.Parameters()
+
+	parametersStream := nex.NewStreamIn(parameters, matchMakingProtocol.server)
+
+	gatheringId := parametersStream.ReadUInt32LE()
+
+	go matchMakingProtocol.UpdateSessionHostHandler(nil, client, callID, gatheringId)
 }
 
 // NewMatchMakingProtocol returns a new MatchMakingProtocol

c5ea4bf1076ed85bba412ead447bf154726cf0c3
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-05-22 11:50:06 -0400

    Added Copy and Equals methods to Notifications types

--- notifications/types.go
+++ notifications/types.go
@@ -14,20 +14,65 @@ type NotificationEvent struct {
 }
 
 // Bytes encodes the NotificationEvent and returns a byte array
-func (notificationEventGeneral *NotificationEvent) Bytes(stream *nex.StreamOut) []byte {
+func (notificationEvent *NotificationEvent) Bytes(stream *nex.StreamOut) []byte {
 	nexVersion := stream.Server.NEXVersion()
 
-	stream.WriteUInt32LE(notificationEventGeneral.PIDSource)
-	stream.WriteUInt32LE(notificationEventGeneral.Type)
-	stream.WriteUInt32LE(notificationEventGeneral.Param1)
-	stream.WriteUInt32LE(notificationEventGeneral.Param2)
-	stream.WriteString(notificationEventGeneral.StrParam)
+	stream.WriteUInt32LE(notificationEvent.PIDSource)
+	stream.WriteUInt32LE(notificationEvent.Type)
+	stream.WriteUInt32LE(notificationEvent.Param1)
+	stream.WriteUInt32LE(notificationEvent.Param2)
+	stream.WriteString(notificationEvent.StrParam)
 
 	if nexVersion.Major >= 3 && nexVersion.Minor >= 5 {
-		stream.WriteUInt32LE(notificationEventGeneral.Param3)
+		stream.WriteUInt32LE(notificationEvent.Param3)
 	}
 
 	return stream.Bytes()
+}
+
+// Copy returns a new copied instance of NotificationEvent
+func (notificationEvent *NotificationEvent) Copy() nex.StructureInterface {
+	copied := NewNotificationEvent()
+
+	copied.PIDSource = notificationEvent.PIDSource
+	copied.Type = notificationEvent.Type
+	copied.Param1 = notificationEvent.Param1
+	copied.Param2 = notificationEvent.Param2
+	copied.StrParam = notificationEvent.StrParam
+	copied.Param3 = notificationEvent.Param3
+
+	return copied
+}
+
+// Equals checks if the passed Structure contains the same data as the current instance
+func (notificationEvent *NotificationEvent) Equals(structure nex.StructureInterface) bool {
+	other := structure.(*NotificationEvent)
+
+	if notificationEvent.PIDSource != other.PIDSource {
+		return false
+	}
+
+	if notificationEvent.Type != other.Type {
+		return false
+	}
+
+	if notificationEvent.Param1 != other.Param1 {
+		return false
+	}
+
+	if notificationEvent.Param2 != other.Param2 {
+		return false
+	}
+
+	if notificationEvent.StrParam != other.StrParam {
+		return false
+	}
+
+	if notificationEvent.Param3 != other.Param3 {
+		return false
+	}
+
+	return true
 }
 
 // NotificationEvent returns a new NotificationEvent

4b7aa6aa62b7bace37add3ea835871fa51abf9e4
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-05-22 11:50:14 -0400

    Added Copy and Equals methods to NintendoNotifications types

--- nintendo-notifications/types.go
+++ nintendo-notifications/types.go
@@ -21,6 +21,41 @@ func (nintendoNotificationEventGeneral *NintendoNotificationEventGeneral) Bytes(
 	return stream.Bytes()
 }
 
+// Copy returns a new copied instance of NintendoNotificationEventGeneral
+func (nintendoNotificationEventGeneral *NintendoNotificationEventGeneral) Copy() nex.StructureInterface {
+	copied := NewNintendoNotificationEventGeneral()
+
+	copied.U32Param = nintendoNotificationEventGeneral.U32Param
+	copied.U64Param1 = nintendoNotificationEventGeneral.U64Param1
+	copied.U64Param2 = nintendoNotificationEventGeneral.U64Param2
+	copied.StrParam = nintendoNotificationEventGeneral.StrParam
+
+	return copied
+}
+
+// Equals checks if the passed Structure contains the same data as the current instance
+func (nintendoNotificationEventGeneral *NintendoNotificationEventGeneral) Equals(structure nex.StructureInterface) bool {
+	other := structure.(*NintendoNotificationEventGeneral)
+
+	if nintendoNotificationEventGeneral.U32Param != other.U32Param {
+		return false
+	}
+
+	if nintendoNotificationEventGeneral.U64Param1 != other.U64Param1 {
+		return false
+	}
+
+	if nintendoNotificationEventGeneral.U64Param2 != other.U64Param2 {
+		return false
+	}
+
+	if nintendoNotificationEventGeneral.StrParam != other.StrParam {
+		return false
+	}
+
+	return true
+}
+
 // NintendoNotificationEventGeneral returns a new NintendoNotificationEventGeneral
 func NewNintendoNotificationEventGeneral() *NintendoNotificationEventGeneral {
 	return &NintendoNotificationEventGeneral{}
@@ -41,6 +76,36 @@ func (nintendoNotificationEvent *NintendoNotificationEvent) Bytes(stream *nex.St
 	stream.WriteDataHolder(nintendoNotificationEvent.DataHolder)
 
 	return stream.Bytes()
+}
+
+// Copy returns a new copied instance of NintendoNotificationEvent
+func (nintendoNotificationEvent *NintendoNotificationEvent) Copy() nex.StructureInterface {
+	copied := NewNintendoNotificationEvent()
+
+	copied.Type = nintendoNotificationEvent.Type
+	copied.SenderPID = nintendoNotificationEvent.SenderPID
+	copied.DataHolder = nintendoNotificationEvent.DataHolder.Copy()
+
+	return copied
+}
+
+// Equals checks if the passed Structure contains the same data as the current instance
+func (nintendoNotificationEvent *NintendoNotificationEvent) Equals(structure nex.StructureInterface) bool {
+	other := structure.(*NintendoNotificationEvent)
+
+	if nintendoNotificationEvent.Type != other.Type {
+		return false
+	}
+
+	if nintendoNotificationEvent.SenderPID != other.SenderPID {
+		return false
+	}
+
+	if !nintendoNotificationEvent.DataHolder.Equals(other.DataHolder) {
+		return false
+	}
+
+	return true
 }
 
 // NewNintendoNotificationEvent returns a new NintendoNotificationEvent

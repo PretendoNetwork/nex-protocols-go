6674dd418260564ef4da186cbaa7c77e75b46dfd
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-05-23 21:57:59 -0400

    Added NotificationTypes struct to Notifications and Nintendo Notifications protocols

---
+++ nintendo-notifications/notification_types.go
@@ -0,0 +1,43 @@
+package nintendo_notifications
+
+type notificationTypes struct {
+	FriendOffline                          uint32
+	FriendMiiChange                        uint32
+	Unknown1MiiRelated                     uint32
+	FriendPreferencesChanged               uint32
+	FriendStartedTitle                     uint32
+	Unknown2FriendRequestRelated           uint32
+	FriendRemoved                          uint32 // * These 2 are the same event. Split them into 2 for cleaner naming
+	FriendRequestCanceled                  uint32 // * These 2 are the same event. Split them into 2 for cleaner naming
+	FriendRequestReceived                  uint32
+	Unknown3FriendRequestRelated           uint32
+	Unknown4BlacklistRelated               uint32
+	FriendRequestAccepted                  uint32
+	Unknown5BlacklistRelated               uint32
+	Unknown6BlacklistRelated               uint32
+	FriendStatusMessageChanged             uint32
+	Unknown7                               uint32
+	Unknown8FriendshipRelated              uint32
+	Unknown9PersistentNotificationsRelated uint32
+}
+
+var NotificationTypes = notificationTypes{
+	FriendOffline:                          10,
+	FriendMiiChange:                        21,
+	Unknown1MiiRelated:                     22,
+	FriendPreferencesChanged:               23,
+	FriendStartedTitle:                     24,
+	Unknown2FriendRequestRelated:           25,
+	FriendRemoved:                          26, // * These 2 are the same event. Split them into 2 for cleaner naming
+	FriendRequestCanceled:                  26, // * These 2 are the same event. Split them into 2 for cleaner naming
+	FriendRequestReceived:                  27,
+	Unknown3FriendRequestRelated:           28,
+	Unknown4BlacklistRelated:               29,
+	FriendRequestAccepted:                  30,
+	Unknown5BlacklistRelated:               31,
+	Unknown6BlacklistRelated:               32,
+	FriendStatusMessageChanged:             33,
+	Unknown7:                               34,
+	Unknown8FriendshipRelated:              35,
+	Unknown9PersistentNotificationsRelated: 36,
+}
---
+++ notifications/notification_types.go
@@ -0,0 +1,29 @@
+package notifications
+
+type notificationTypes struct {
+	NewParticipant               uint32
+	ParticipationCancelled       uint32
+	ParticipantDisconnected      uint32
+	ParticipationEnded           uint32
+	OwnershipChanged             uint32
+	GatheringUnregistered        uint32
+	HostChanged                  uint32
+	MatchmakeRefereeRoundStarted uint32
+	SystemPasswordChanged        uint32
+	SystemPasswordCleared        uint32
+	SwitchGathering              uint32
+}
+
+var NotificationTypes = notificationTypes{
+	NewParticipant:               3001,
+	ParticipationCancelled:       3002,
+	ParticipantDisconnected:      3007,
+	ParticipationEnded:           3008,
+	OwnershipChanged:             4000,
+	GatheringUnregistered:        109000,
+	HostChanged:                  110000,
+	MatchmakeRefereeRoundStarted: 116000,
+	SystemPasswordChanged:        120000,
+	SystemPasswordCleared:        121000,
+	SwitchGathering:              122000,
+}

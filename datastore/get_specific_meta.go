1060ebc8e38583d3c1e0c2b9d7fb67da130389e9
Daniel López Guimaraes <danielectra@outlook.com>
2023-07-17 13:54:24 +0100

    Add DataStore.GetSpecificMeta(V1) method

---
+++ datastore/get_specific_meta.go
@@ -0,0 +1,39 @@
+// Package datastore implements the DataStore NEX protocol
+package datastore
+
+import (
+	"fmt"
+
+	nex "github.com/PretendoNetwork/nex-go"
+	datastore_types "github.com/PretendoNetwork/nex-protocols-go/datastore/types"
+	"github.com/PretendoNetwork/nex-protocols-go/globals"
+)
+
+// GetSpecificMeta sets the GetSpecificMeta handler function
+func (protocol *DataStoreProtocol) GetSpecificMeta(handler func(err error, client *nex.Client, callID uint32, param *datastore_types.DataStoreGetSpecificMetaParam)) {
+	protocol.GetSpecificMetaHandler = handler
+}
+
+func (protocol *DataStoreProtocol) handleGetSpecificMeta(packet nex.PacketInterface) {
+	if protocol.PrepareGetObjectV1Handler == nil {
+		globals.Logger.Warning("DataStore::GetSpecificMeta not implemented")
+		go globals.RespondNotImplemented(packet, ProtocolID)
+		return
+	}
+
+	client := packet.Sender()
+	request := packet.RMCRequest()
+
+	callID := request.CallID()
+	parameters := request.Parameters()
+
+	parametersStream := nex.NewStreamIn(parameters, protocol.Server)
+
+	param, err := parametersStream.ReadStructure(datastore_types.NewDataStoreGetSpecificMetaParam())
+	if err != nil {
+		go protocol.GetSpecificMetaHandler(fmt.Errorf("Failed to read dataStoreGetSpecificMetaParam from parameters. %s", err.Error()), client, callID, nil)
+		return
+	}
+
+	go protocol.GetSpecificMetaHandler(nil, client, callID, param.(*datastore_types.DataStoreGetSpecificMetaParam))
+}

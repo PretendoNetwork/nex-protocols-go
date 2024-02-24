9a0f5efc5ff2e48b00289dc91ce1b1e87d6e20d5
Daniel López Guimaraes <danielectra@outlook.com>
2024-02-24 00:20:32 +0000

    matchmake-extension: Update JoinMatchmakeSessionEx args
    Check for NEX version 3.4.0 or later for participation count.

--- matchmake-extension/join_matchmake_session_ex.go
+++ matchmake-extension/join_matchmake_session_ex.go
@@ -23,6 +23,7 @@ func (protocol *Protocol) handleJoinMatchmakeSessionEx(packet nex.PacketInterfac
 	callID := request.CallID
 	parameters := request.Parameters
 	endpoint := packet.Sender().Endpoint()
+	matchmakingVersion := endpoint.LibraryVersions().MatchMaking
 	parametersStream := nex.NewByteStreamIn(parameters, endpoint.LibraryVersions(), endpoint.ByteStreamSettings())
 
 	gid := types.NewPrimitiveU32(0)
@@ -62,14 +63,16 @@ func (protocol *Protocol) handleJoinMatchmakeSessionEx(packet nex.PacketInterfac
 		return
 	}
 
-	err = participationCount.ExtractFrom(parametersStream)
-	if err != nil {
-		_, rmcError := protocol.JoinMatchmakeSessionEx(fmt.Errorf("Failed to read participationCount from parameters. %s", err.Error()), packet, callID, nil, nil, nil, nil)
-		if rmcError != nil {
-			globals.RespondError(packet, ProtocolID, rmcError)
-		}
+	if matchmakingVersion.GreaterOrEqual("3.4.0") {
+		err = participationCount.ExtractFrom(parametersStream)
+		if err != nil {
+			_, rmcError := protocol.JoinMatchmakeSessionEx(fmt.Errorf("Failed to read participationCount from parameters. %s", err.Error()), packet, callID, nil, nil, nil, nil)
+			if rmcError != nil {
+				globals.RespondError(packet, ProtocolID, rmcError)
+			}
 
-		return
+			return
+		}
 	}
 
 	rmcMessage, rmcError := protocol.JoinMatchmakeSessionEx(nil, packet, callID, gid, strMessage, dontCareMyBlockList, participationCount)

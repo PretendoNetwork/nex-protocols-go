d0d5b5f6c2876ab6c9d7bd40c3332eee97b3105c
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-02-16 11:54:01 -0500

    chore: RatingLockPeriodType to RatingLockPeriodDay

--- datastore/constants/rating_lock_period.go
+++ datastore/constants/rating_lock_period.go
@@ -1,39 +1,44 @@
 package constants
 
 // RatingLockPeriod tells the rating slot locks what day
-// the lock should expire
-type RatingLockPeriodType int16
+// the lock should expire.
+//
+// NOTE: Original name is RatingLockPeriod, changed to
+// RatingLockPeriodDay as that conflicts with
+// RatingLockType.RatingLockPeriod, and these values
+// represent the "day" a period lock ends.
+type RatingLockPeriodDay int16
 
 const (
 	// RatingLockPeriodDay1 means the day the lock will expire
 	// is the 1st of the following month
-	RatingLockPeriodDay1 RatingLockPeriodType = -17
+	RatingLockPeriodDay1 RatingLockPeriodDay = -17
 
 	// RatingLockPeriodSun means the day the lock will expire
 	// is the Sunday of the following week
-	RatingLockPeriodSun RatingLockPeriodType = -7
+	RatingLockPeriodSun RatingLockPeriodDay = -7
 
 	// RatingLockPeriodSat means the day the lock will expire
 	// is the Saturday of the following week
-	RatingLockPeriodSat RatingLockPeriodType = -6
+	RatingLockPeriodSat RatingLockPeriodDay = -6
 
 	// RatingLockPeriodFri means the day the lock will expire
 	// is the Friday of the following week
-	RatingLockPeriodFri RatingLockPeriodType = -5
+	RatingLockPeriodFri RatingLockPeriodDay = -5
 
 	// RatingLockPeriodThu means the day the lock will expire
 	// is the Thursday of the following week
-	RatingLockPeriodThu RatingLockPeriodType = -4
+	RatingLockPeriodThu RatingLockPeriodDay = -4
 
 	// RatingLockPeriodWed means the day the lock will expire
 	// is the Wednesday of the following week
-	RatingLockPeriodWed RatingLockPeriodType = -3
+	RatingLockPeriodWed RatingLockPeriodDay = -3
 
 	// RatingLockPeriodTue means the day the lock will expire
 	// is the Tuesday of the following week
-	RatingLockPeriodTue RatingLockPeriodType = -2
+	RatingLockPeriodTue RatingLockPeriodDay = -2
 
 	// RatingLockPeriodMon means the day the lock will expire
 	// is the Monday of the following week
-	RatingLockPeriodMon RatingLockPeriodType = -1
+	RatingLockPeriodMon RatingLockPeriodDay = -1
 )

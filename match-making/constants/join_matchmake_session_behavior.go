47f9eb1da6978d4d237aeb62284d5534ce525238
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-07-09 00:33:05 -0400

    chore(match-making): add usage details to JoinMatchmakeSessionBehaviorImAlreadyJoined

--- match-making/constants/join_matchmake_session_behavior.go
+++ match-making/constants/join_matchmake_session_behavior.go
@@ -4,9 +4,12 @@ package constants
 type JoinMatchmakeSessionBehavior uint8
 
 const (
-	// JoinMatchmakeSessionBehaviorJoinMyself indicates that the caller wants to join the session
+	// JoinMatchmakeSessionBehaviorJoinMyself indicates that the caller wants to join the session.
 	JoinMatchmakeSessionBehaviorJoinMyself JoinMatchmakeSessionBehavior = iota
 
-	// JoinMatchmakeSessionBehaviorImAlreadyJoined indicates that the caller is already joined into the session
+	// JoinMatchmakeSessionBehaviorImAlreadyJoined indicates that the caller is already joined into the session.
+	//
+	// This can be useful in cases where the caller wants to add additional users to the session using additionalParticipants
+	// without trying to join the session themselves multiple times.
 	JoinMatchmakeSessionBehaviorImAlreadyJoined
 )

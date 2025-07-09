eba96dbde0e49732982b73d47a6919a0bf8d96f5
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-07-09 00:28:15 -0400

    feat(match-making): add MatchmakeOption enum

---
+++ match-making/constants/matchmake_option.go
@@ -0,0 +1,17 @@
+package constants
+
+// MatchmakeOption has an unknown use.
+type MatchmakeOption uint32
+
+const (
+	// MatchmakeOptionNone has an unknown use.
+	MatchmakeOptionNone MatchmakeOption = iota
+
+	// MatchmakeOptionRecordLastGIDForParticipationCheck has an unknown use.
+	//
+	// Possibly related to gidForParticipationCheck?
+	MatchmakeOptionRecordLastGIDForParticipationCheck
+
+	// MatchmakeOptionReserved1 has an unknown use.
+	MatchmakeOptionReserved1
+)

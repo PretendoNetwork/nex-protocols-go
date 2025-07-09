dd7c003aa3623ca5e26899a11ab27d5ff60f438e
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-07-09 02:29:42 -0400

    fix(match-making): fix AnybodyParticipationPolicyArgument typos

--- match-making/constants/anybody_particiation_policy_argument.go
+++ match-making/constants/anybody_particiation_policy_argument.go
@@ -2,15 +2,15 @@ package constants
 
 // AnybodyParticipationPolicyArgument seems to determine whether or not to
 // close participation when the gathering owner changes when MatchmakeSystemTypeAnybody
-// is used.
+// is used?
 type AnybodyParticipationPolicyArgument uint32
 
 const (
 	// AnybodyParticipationPolicyArgumentWithoutClose means that participation should not
-	// close when a new host is selected.
+	// close when a new owner is selected.
 	AnybodyParticipationPolicyArgumentWithoutClose AnybodyParticipationPolicyArgument = iota
 
 	// AnybodyParticipationPolicyArgumentCloseOnOwnerMigration means that participation should
-	// close when a new host is selected.
+	// close when a new owner is selected.
 	AnybodyParticipationPolicyArgumentCloseOnOwnerMigration
 )

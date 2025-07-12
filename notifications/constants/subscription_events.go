23caa16f3c700b7fba43ca5ff36595604f1f1008
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-07-12 00:44:02 -0400

    feat(notifications): add SubscriptionEvents

---
+++ notifications/constants/subscription_events.go
@@ -0,0 +1,30 @@
+package constants
+
+// SubscriptionEvents represents the various subtypes of NotificationEventsSubscriptionEvent
+//
+// Note: Name is a guess, as we don't know the real name. Guess is based
+// on the structure of ParticipationEvents.
+type SubscriptionEvents = subType
+
+const (
+	// SubscriptionEventsEvent0 has an unknown use.
+	//
+	// Details unknown.
+	//
+	// Parameters unknown.
+	SubscriptionEventsEvent0 SubscriptionEvents = 0
+
+	// SubscriptionEventsEvent1 has an unknown use.
+	//
+	// Details unknown.
+	//
+	// Parameters unknown.
+	SubscriptionEventsEvent1 SubscriptionEvents = 1
+
+	// SubscriptionEventsEvent2 has an unknown use.
+	//
+	// Details unknown.
+	//
+	// Parameters unknown.
+	SubscriptionEventsEvent2 SubscriptionEvents = 2
+)

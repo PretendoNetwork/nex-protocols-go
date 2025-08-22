4c269492afc2bee569727de82cf3ee433824f7ed
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-08-22 18:58:19 -0400

    feat(ranking): add missing ranking constants

---
+++ ranking/constants/constants.go
@@ -0,0 +1,17 @@
+package constants
+
+const (
+	// MaxCommonDataSize is the maximum length a users common data may be.
+	MaxCommonDataSize uint8 = 255
+
+	// MaxRangeRankingOrder is the maximum value an offset may be when
+	// requesting ranking data.
+	MaxRangeRankingOrder uint32 = 1000
+
+	// MaxAccurateOrder has an unknown use. This likely is used to indicate
+	// the largest number of ranking results that have accurate ranking orders?
+	//
+	// For example, if 4999 rankings are returned, the client knows that this
+	// data has accurate rankings. but if 5001 are returned, they may not be?
+	MaxAccurateOrder uint32 = 5000
+)

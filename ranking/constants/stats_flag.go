807c62e1fc176bbd1632f7927ac5e77e4b9a482c
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-08-27 16:40:36 -0400

    chore(ranking): add HasFlag and HasFlags methods to flag constants

--- ranking/constants/stats_flag.go
+++ ranking/constants/stats_flag.go
@@ -5,6 +5,42 @@ import "github.com/PretendoNetwork/nex-go/v2/types"
 // StatsFlag is a bitmask used by GetRanking to request the inclusion of different aggregate stats
 type StatsFlag uint32
 
+// WriteTo writes the StatsFlag to the given writable
+func (sf StatsFlag) WriteTo(writable types.Writable) {
+	writable.WriteUInt32LE(uint32(sf))
+}
+
+// ExtractFrom extracts the StatsFlag value from the given readable
+func (sf *StatsFlag) ExtractFrom(readable types.Readable) error {
+	value, err := readable.ReadUInt32LE()
+	if err != nil {
+		return err
+	}
+
+	*sf = StatsFlag(value)
+	return nil
+}
+
+// HasFlag checks if a given flag is set
+func (sf StatsFlag) HasFlag(flag StatsFlag) bool {
+	return sf&flag == flag
+}
+
+// HasFlag checks if all given flags are set
+func (sf StatsFlag) HasFlags(flags ...StatsFlag) bool {
+	if len(flags) == 0 {
+		return false
+	}
+
+	for _, flag := range flags {
+		if sf&flag != flag {
+			return false
+		}
+	}
+
+	return true
+}
+
 const (
 	// StatsFlagTotal requests the total of the stats
 	StatsFlagTotal = 0x1
@@ -21,19 +57,3 @@ const (
 	// StatsFlagAverage requests the average of the stats
 	StatsFlagAverage = 0x10
 )
-
-// WriteTo writes the StatsFlag to the given writable
-func (sf StatsFlag) WriteTo(writable types.Writable) {
-	writable.WriteUInt32LE(uint32(sf))
-}
-
-// ExtractFrom extracts the StatsFlag value from the given readable
-func (sf *StatsFlag) ExtractFrom(readable types.Readable) error {
-	value, err := readable.ReadUInt32LE()
-	if err != nil {
-		return err
-	}
-
-	*sf = StatsFlag(value)
-	return nil
-}

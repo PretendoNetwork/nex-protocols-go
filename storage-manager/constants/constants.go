1fe7bd19067054fc84e40a40454bbaf6b2ace8b8
Daniel López Guimaraes <danielectra@outlook.com>
2026-01-28 20:11:33 +0000

    feat(storage-manager): add constants

---
+++ storage-manager/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	// StorageManagerSlotsMax is the maximum number of slots
+	// that a user and card ID can have assigned
+	//
+	// Note: This name is a guess based on "DatastorePermissionRecipientIDsMax"
+	StorageManagerSlotsMax int = 5
+)
